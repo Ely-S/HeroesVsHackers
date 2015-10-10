75295e8ca9a637108e7882763b486330c88a63f1
Freeman Lou <freemanlou2430@yahoo.com>
2015-10-10 16:09:38 -0400

    Added test API get user function with fake data

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,16 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res){
-	res.status(200).send('some res');
+app.get('/user/:id', function(req, res){
+	var id = req.params.id;
+	var path = './data/data.json';
+	console.log(id);
+	
+	var content = fs.readFileSync(path);
+
+	res.send(JSON.parse(content));
 });
+
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Server listening on port ' + app.get('port'));
