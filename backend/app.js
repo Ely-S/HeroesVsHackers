f5b95794f4fdaa6a82e0660db5ec0c1395197aca
Freeman Lou <freemanlou2430@yahoo.com>
2015-10-10 20:58:23 -0400

    added retailer route

--- backend/app.js
+++ backend/app.js
@@ -4,14 +4,16 @@ var path 		= require('path');
 var url 		= require('url');
 var fs			= require('fs');
 
+var person 		= require('./person');
+
 var app = express(),
 	db = JSON.parse(fs.readFileSync("./data/data.json"));
 
 app.set('port', process.env.PORT || 3000);
 
 
-app.get('/user/:id', function(req, res){
-	var content = db[req.params.id];
+app.get('/user', function(req, res){
+	var content = db[req.user.id];
 	if (content) {
 		res.send(content);
 	} else {
@@ -19,12 +21,34 @@ app.get('/user/:id', function(req, res){
 	}
 });
 
-app.put('/', function(req, res) {
+app.put('/user/:id', function(req, res) {
+	// var id = req.user.id;
+	// var name = req.user.name;
 
+	// db.push(new person.Person(id,name));
+	// console.log(db);
 });
 
-app.post('/', function(req, res) {
+app.put('/user/:id/:retailer', function(req, res) {
+	var key = req.query['key']
+	var newPoints = req.query['points'];
 
+	var id = req.params['id'];
+	var retailer = req.params['retailer'];
+
+	//Needs retailer authentication
+	if(key) {
+		db[id].user_monsters.find(function(element){
+			return element.company == retailer;
+		}).points = newPoints;
+
+		fs.writeFile("./data/data.json", JSON.stringify(db), function() {
+			res.send("successfully modified");			
+		});
+
+	} else {
+		res.status(401).end();
+	}
 });
 
 http.createServer(app).listen(app.get('port'), function() {
