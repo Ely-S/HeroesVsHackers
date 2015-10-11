74d2fea339a81de4103a4353de6b8ef8fd6d5ae4
Mahathi Vootla Krishna <kris.mahathi@gmail.com>
2015-10-10 23:55:53 -0400

    form validation

---
+++ frontend/js/validate.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Mahathi on 10/10/2015.
+ */
+function validateSignup(){
+    var m = document.forms["signup"]["user"].value;
+    var x = document.forms["signup"]["pwd"].value;
+    var a = document.forms["signup"]["pwd2"].value;
+    if(m == null || m == "")
+    {
+        alert("Enter email");
+        return false;
+    }
+
+    if ((x == null || x == "")&& x.length < 6) {
+        alert("Enter password");
+        return false;
+    }
+    if(a != x && a.length < 6)
+    {
+        alert("Passwords do not match. Enter correct password");
+        return false;
+    }
+}
+
+function validateSignin(){
+    var p = document.forms["signin"]["email"].value;
+    var r = document.forms["signin"]["password"].value;
+    if(p == null || p == "")
+    {
+        alert("Enter email");
+        return false;
+    }
+    if((r == null || r == "") && r.length<6)
+    {
+        alert("Enter password");
+        return false;
+    }
+}
